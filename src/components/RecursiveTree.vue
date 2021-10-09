4a64cc95dca8303220c7af92d354624b64674974
Mason Hsieh <benius@gmail.com>
2021-10-09 14:35:10 +0800

    Add RecursiveTree component

---
+++ src/components/RecursiveTree.vue
@@ -0,0 +1,76 @@
+<template>
+  <ul>
+    <li>
+      <template v-if='treeNode.children && treeNode.children.length>0'>
+        <h4 class='has-child'
+            :class="{ 'is-open': openState.isOpen }"
+            @click='openState.isOpen = !openState.isOpen'>
+          {{ treeNode.title }}
+        </h4>
+        <recursive-tree v-show='openState.isOpen'
+                        v-for='child in treeNode.children'
+                        :key='child.title'
+                        :tree-node='child'>
+        </recursive-tree>
+      </template>
+      <a v-else :href='treeNode.url' target='_blank'>{{ treeNode.title }}</a>
+    </li>
+  </ul>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType, reactive } from 'vue';
+import { TreeNode } from 'components/models';
+
+export default defineComponent ({
+  name: 'RecursiveTree',
+  props: {
+    treeNode: {
+      type: Object as PropType<TreeNode>,
+      default: () => ({
+        title: '',
+        url: '',
+        children: []
+      })
+    }
+  },
+  setup() {
+    let openState = reactive({
+      isOpen: false
+    })
+
+    return {
+      openState
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  list-style-type: none; 
+}
+
+h4.has-child {
+  cursor: pointer;
+}
+
+h4.has-child::before {
+  content: '+ ';
+}
+
+h4.has-child.is-open::before {
+  content: '- ';
+}
+
+a {
+  color: #333;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+}
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,17 +6,27 @@
       :todos="todos"
       :meta="meta"
     ></example-component>
+
+    <!-- RecursiveTree -->
+    <div>
+      <recursive-tree v-show='true'
+                      v-for='book in books'
+                      :key='book.title'
+                      :tree-node='book'>
+      </recursive-tree>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
+import { Todo, Meta, TreeNode } from 'components/models';
 import ExampleComponent from 'components/CompositionComponent.vue';
+import RecursiveTree from 'components/RecursiveTree.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
+  components: { ExampleComponent, RecursiveTree },
   setup() {
     const todos = ref<Todo[]>([
       {
@@ -43,7 +53,102 @@ export default defineComponent({
     const meta = ref<Meta>({
       totalCount: 1200
     });
-    return { todos, meta };
+
+    const books = loadBookData();
+
+    function loadBookData(): TreeNode[] {
+      return [
+        {
+          title: '好書推薦',
+          children: [
+            {
+              title: 'Git',
+              children: [{
+                title: '為你自己學 Git',
+                url: 'https://www.tenlong.com.tw/products/9789864342662'
+              }]
+            },
+            {
+              title: '前端開發',
+              children: [
+                {
+                  title: '金魚都能懂的 CSS 選取器',
+                  url: 'https://www.tenlong.com.tw/products/9789864344994'
+                },
+                {
+                  title: '0 陷阱！0 誤解！8 天重新認識 JavaScript！',
+                  url: 'https://www.tenlong.com.tw/products/9789864344130'
+                },
+                {
+                  title: '讓 TypeScript 成為你全端開發的 ACE！',
+                  url: 'https://www.tenlong.com.tw/products/9789864344895'
+                }
+              ]
+            },
+            {
+              title: 'IoT',
+              children: [{
+                title: 'IoT沒那麼難！新手用 JavaScript 入門做自己的玩具！',
+                url: 'https://www.tenlong.com.tw/products/9789864345328'
+              }]
+            },
+            {
+              title: 'Chatbot',
+              children: [{
+                title: '人人可作卡米狗：從零打造自己的 LINE 聊天機器人',
+                url: 'https://www.tenlong.com.tw/products/9789864342938'
+              }]
+            }
+          ]
+        },
+
+        {
+          title: '好書推薦2',
+          children: [
+            {
+              title: 'Git2',
+              children: [{
+                title: '為你自己學 Git',
+                url: 'https://www.tenlong.com.tw/products/9789864342662'
+              }]
+            },
+            {
+              title: '前端開發2',
+              children: [
+                {
+                  title: '金魚都能懂的 CSS 選取器',
+                  url: 'https://www.tenlong.com.tw/products/9789864344994'
+                },
+                {
+                  title: '0 陷阱！0 誤解！8 天重新認識 JavaScript！',
+                  url: 'https://www.tenlong.com.tw/products/9789864344130'
+                },
+                {
+                  title: '讓 TypeScript 成為你全端開發的 ACE！',
+                  url: 'https://www.tenlong.com.tw/products/9789864344895'
+                }
+              ]
+            },
+            {
+              title: 'IoT2',
+              children: [{
+                title: 'IoT沒那麼難！新手用 JavaScript 入門做自己的玩具！',
+                url: 'https://www.tenlong.com.tw/products/9789864345328'
+              }]
+            },
+            {
+              title: 'Chatbot2',
+              children: [{
+                title: '人人可作卡米狗：從零打造自己的 LINE 聊天機器人',
+                url: 'https://www.tenlong.com.tw/products/9789864342938'
+              }]
+            }
+          ]
+        }
+      ];
+    }
+
+    return { todos, meta, books };
   }
 });
 </script>
